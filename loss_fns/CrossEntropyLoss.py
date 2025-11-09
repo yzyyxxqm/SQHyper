4a457be5988542fb1731135888a695d67da5d486
Ladbaby <1390265970@qq.com>
2025-11-09 22:22:22 +0800

    feat(loss):loss_reduction_none

--- loss_fns/CrossEntropyLoss.py
+++ loss_fns/CrossEntropyLoss.py
@@ -1,20 +1,32 @@
 # Code from: https://github.com/Ladbaby/PyOmniTS
 import torch
 import torch.nn as nn
+from torch import Tensor
 
 from utils.ExpConfigs import ExpConfigs
+from utils.globals import logger
 
 class Loss(nn.Module):
     def __init__(self, configs:ExpConfigs):
         super(Loss, self).__init__()
-        self.criterion = nn.CrossEntropyLoss()
+        self.criterion = nn.CrossEntropyLoss(reduction="none")
 
-    def forward(self, pred_class, true_class, **kwargs):
+    def forward(
+        self, 
+        pred_class: Tensor, 
+        true_class: Tensor, 
+        **kwargs
+    ) -> dict[str, Tensor]:
         '''
         - pred_class: [BATCH_SIZE, N_CLASSES] torch.float32
         - true_class: [BATCH_SIZE, N_CLASSES] torch.float32
             should be converted to LongTensor of shape [BATCH_SIZE], which means dtype of torch.int64
         '''
+        if pred_class.shape != true_class.shape:
+            logger.exception(f"CrossEntropyLoss expects pred_class and true_class in the same shape. Currently, {pred_class.shape=} while {true_class.shape=}. This may be caused by an incorrect setting or usage of --n_classes")
+            exit(1)
+        loss_reduction_none = self.criterion(pred_class, torch.argmax(true_class, dim=1).to(pred_class.device))
         return {
-            "loss": self.criterion(pred_class, torch.argmax(true_class, dim=1).to(pred_class.device))
+            "loss": loss_reduction_none.mean(),
+            "loss_reduction_none": loss_reduction_none # same shape as pred_class
         }
--- loss_fns/MSE.py
+++ loss_fns/MSE.py
@@ -1,6 +1,7 @@
 # Code from: https://github.com/Ladbaby/PyOmniTS
 import torch
 import torch.nn as nn
+from torch import Tensor
 
 from utils.ExpConfigs import ExpConfigs
 
@@ -8,13 +9,24 @@ class Loss(nn.Module):
     def __init__(self, configs:ExpConfigs):
         super(Loss, self).__init__()
 
-    def forward(self, pred, true, mask=None, **kwargs):
+    def forward(
+        self, 
+        pred: Tensor, 
+        true: Tensor, 
+        mask: Tensor | None = None, 
+        **kwargs
+    ) -> dict[str, Tensor]:
+        # BEGIN adaptor
         if mask is None:
             mask = torch.ones_like(true, device=true.device)
+        # END adaptor
 
         residual = (pred - true) * mask
+        squared_error = residual ** 2
         num_eval = mask.sum()
+        loss = squared_error.sum() / (num_eval if num_eval > 0 else 1)
 
         return {
-            "loss": (residual ** 2).sum() / (num_eval if num_eval > 0 else 1)
+            "loss": loss,
+            "loss_reduction_none": squared_error # same shape as pred
         }

900c632672d0c456eb1eba248039abf1d50bd104
Ladbaby <1390265970@qq.com>
2026-01-06 17:01:29 +0800

    fix(model):TiDE

--- models/TiDE.py
+++ models/TiDE.py
@@ -33,29 +33,19 @@ class Model(nn.Module):
         self.temporalDecoderHidden=configs.d_ff
         dropout=configs.dropout
 
-        
-        # freq_map = {'h': 4, 't': 5, 's': 6,
-        #             'm': 1, 'a': 1, 'w': 2, 'd': 3, 'b': 3}
-        
-        # self.feature_dim=freq_map[self.freq]
-        self.feature_dim = configs.enc_in
-
+        # self.feature_dim is removed, and LazyLinear is used to adapt to y_marks' last dimension size in ResBlock
 
         flatten_dim = self.seq_len + (self.seq_len + self.pred_len) * self.feature_encode_dim
 
-        self.feature_encoder = ResBlock(self.feature_dim, self.res_hidden, self.feature_encode_dim, dropout, bias)
-        self.encoders = nn.Sequential(ResBlock(flatten_dim, self.res_hidden, self.hidden_dim, dropout, bias),*([ ResBlock(self.hidden_dim, self.res_hidden, self.hidden_dim, dropout, bias)]*(self.encoder_num-1)))
-        if self.task_name in ['long_term_forecast', 'short_term_forecast']:
-            self.decoders = nn.Sequential(*([ ResBlock(self.hidden_dim, self.res_hidden, self.hidden_dim, dropout, bias)]*(self.decoder_num-1)),ResBlock(self.hidden_dim, self.res_hidden, self.decode_dim * self.pred_len, dropout, bias))
-            self.temporalDecoder = ResBlock(self.decode_dim + self.feature_encode_dim, self.temporalDecoderHidden, 1, dropout, bias)
+        self.feature_encoder = ResBlock(self.res_hidden, self.feature_encode_dim, dropout, bias)
+        self.encoders = nn.Sequential(ResBlock(self.res_hidden, self.hidden_dim, dropout, bias),*([ ResBlock(self.res_hidden, self.hidden_dim, dropout, bias)]*(self.encoder_num-1)))
+        if self.task_name in ['long_term_forecast', 'short_term_forecast', 'imputation']:
+            self.decoders = nn.Sequential(*([ ResBlock(self.res_hidden, self.hidden_dim, dropout, bias)]*(self.decoder_num-1)),ResBlock(self.res_hidden, self.decode_dim * self.pred_len, dropout, bias))
+            self.temporalDecoder = ResBlock(self.temporalDecoderHidden, 1, dropout, bias)
             self.residual_proj = nn.Linear(self.seq_len, self.pred_len, bias=bias)
-        elif self.task_name == 'imputation':
-            self.decoders = nn.Sequential(*([ ResBlock(self.hidden_dim, self.res_hidden, self.hidden_dim, dropout, bias)]*(self.decoder_num-1)),ResBlock(self.hidden_dim, self.res_hidden, self.decode_dim * self.seq_len, dropout, bias))
-            self.temporalDecoder = ResBlock(self.decode_dim + self.feature_encode_dim, self.temporalDecoderHidden, 1, dropout, bias)
-            self.residual_proj = nn.Linear(self.seq_len, self.seq_len, bias=bias)
         elif self.task_name == 'anomaly_detection':
-            self.decoders = nn.Sequential(*([ ResBlock(self.hidden_dim, self.res_hidden, self.hidden_dim, dropout, bias)]*(self.decoder_num-1)),ResBlock(self.hidden_dim, self.res_hidden, self.decode_dim * self.seq_len, dropout, bias))
-            self.temporalDecoder = ResBlock(self.decode_dim + self.feature_encode_dim, self.temporalDecoderHidden, 1, dropout, bias)
+            self.decoders = nn.Sequential(*([ ResBlock(self.res_hidden, self.hidden_dim, dropout, bias)]*(self.decoder_num-1)),ResBlock(self.res_hidden, self.decode_dim * self.seq_len, dropout, bias))
+            self.temporalDecoder = ResBlock(self.temporalDecoderHidden, 1, dropout, bias)
             self.residual_proj = nn.Linear(self.seq_len, self.seq_len, bias=bias)
         elif self.task_name == "classification":
             self.decoder_classification = nn.Linear(self.hidden_dim * configs.enc_in, configs.n_classes)
@@ -78,23 +68,6 @@ class Model(nn.Module):
         # De-Normalization 
         dec_out = dec_out * (stdev[:, 0].unsqueeze(1).repeat(1, self.pred_len))
         dec_out = dec_out + (means[:, 0].unsqueeze(1).repeat(1, self.pred_len))
-        return dec_out
-    
-    def imputation(self, x_enc, x_mark_enc, x_dec, batch_y_mark, mask):
-        # Normalization
-        means = x_enc.mean(1, keepdim=True).detach()
-        x_enc = x_enc - means
-        stdev = torch.sqrt(torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
-        x_enc /= stdev
-
-        feature = self.feature_encoder(x_mark_enc)
-        hidden = self.encoders(torch.cat([x_enc, feature.reshape(feature.shape[0], -1)], dim=-1))
-        decoded = self.decoders(hidden).reshape(hidden.shape[0], self.seq_len, self.decode_dim)
-        dec_out = self.temporalDecoder(torch.cat([feature[:,:self.seq_len], decoded], dim=-1)).squeeze(-1) + self.residual_proj(x_enc)
-    
-        # De-Normalization 
-        dec_out = dec_out * (stdev[:, 0].unsqueeze(1).repeat(1, self.seq_len))
-        dec_out = dec_out + (means[:, 0].unsqueeze(1).repeat(1, self.seq_len))
         return dec_out
     
     def classification(self, x_enc, x_mark_enc):
@@ -133,7 +106,7 @@ class Model(nn.Module):
         if x_mark is None:
             x_mark = repeat(torch.arange(end=x.shape[1], dtype=x.dtype, device=x.device) / x.shape[1], "L -> B L 1", B=x.shape[0])
         if y is None:
-            if self.configs.task_name in ["short_term_forecast", "long_term_forecast"]:
+            if self.configs.task_name in ["short_term_forecast", "long_term_forecast", "imputation"]:
                 logger.warning(f"y is missing for the model input. This is only reasonable when the model is testing flops!")
             y = torch.ones((BATCH_SIZE, Y_LEN, ENC_IN), dtype=x.dtype, device=x.device)
         if y_mark is None:
@@ -147,14 +120,10 @@ class Model(nn.Module):
 
         x_mark[x_mark == 1] = 0.9999 # cannot process value == 1 in mark
         y_mark[y_mark == 1] = 0.9999
-
-        y_mark = repeat(y_mark[:, :, :1], "B L 1 -> B L F", F=x.shape[-1])
-        x_mark = repeat(x_mark[:, :, :1], "B L 1 -> B L F", F=x.shape[-1])
-        '''x_mark_enc is the exogenous dynamic feature described in the original paper'''
-        batch_y_mark=torch.concat([x_mark, y_mark[:, -self.pred_len:, :]],dim=1)
         # END adaptor
 
-        if self.configs.task_name in ['long_term_forecast', 'short_term_forecast']:
+        if self.configs.task_name in ["long_term_forecast", "short_term_forecast", "imputation"]:
+            batch_y_mark=torch.concat([x_mark, y_mark[:, -self.pred_len:, :]],dim=1)
             dec_out = torch.stack([self.forecast(x[:, :, feature], batch_y_mark) for feature in range(x.shape[-1])],dim=-1)
             f_dim = -1 if self.configs.features == 'MS' else 0
             PRED_LEN = y.shape[1]
@@ -185,12 +154,15 @@ class LayerNorm(nn.Module):
 
 
 class ResBlock(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim, dropout=0.1, bias=True): 
+    def __init__(self, hidden_dim, output_dim, dropout=0.1, bias=True): 
+        '''
+        input_dim is removed, and LazyLinear is used to adapt to input size
+        '''
         super().__init__()
 
-        self.fc1 = nn.Linear(input_dim, hidden_dim, bias=bias) 
+        self.fc1 = nn.LazyLinear(hidden_dim, bias=bias) 
         self.fc2 = nn.Linear(hidden_dim, output_dim, bias=bias)
-        self.fc3 = nn.Linear(input_dim, output_dim, bias=bias)
+        self.fc3 = nn.LazyLinear(output_dim, bias=bias)
         self.dropout = nn.Dropout(dropout)
         self.relu = nn.ReLU()
         self.ln = LayerNorm(output_dim, bias=bias)

dca2db530b03da3191da7faafc480511bace812b
Ladbaby <1390265970@qq.com>
2026-01-21 20:23:13 +0800

    feat(scripts):APN

---
+++ scripts/APN/P12.sh
@@ -0,0 +1,49 @@
+use_multi_gpu=0
+if [ $use_multi_gpu -eq 0 ]; then
+    launch_command="python"
+else
+    launch_command="accelerate launch"
+fi
+
+. "$(dirname "$(readlink -f "$0")")/../globals.sh" # Import shared information from scripts/globals.sh
+
+dataset_name=$(basename "$0" .sh) # file name
+dataset_subset_name=""
+dataset_id=$dataset_name
+get_dataset_info "$dataset_name" "$dataset_subset_name" # Get dataset information from scripts/globals.sh
+
+model_name="$(basename "$(dirname "$(readlink -f "$0")")")" # folder name
+model_id=$model_name
+
+seq_len=36
+for pred_len in 3; do
+    $launch_command main.py \
+    --is_training 1 \
+    --collate_fn "collate_fn" \
+    --loss "MSE" \
+    --dropout 0.1 \
+    --d_model 24 \
+    --n_layers 2 \
+    --n_heads 8 \
+    --n_patches_list "20" \
+    --tpatchgnn_te_dim 8 \
+    --use_multi_gpu $use_multi_gpu \
+    --dataset_root_path $dataset_root_path \
+    --model_id $model_id \
+    --model_name $model_name \
+    --dataset_name $dataset_name \
+    --dataset_id $dataset_id \
+    --features M \
+    --seq_len $seq_len \
+    --pred_len $pred_len \
+    --enc_in $n_variables \
+    --c_out $n_variables \
+    --dec_in $n_variables \
+    --train_epochs 300 \
+    --patience 10 \
+    --val_interval 1 \
+    --itr 5 \
+    --batch_size 32 \
+    --learning_rate 3e-2
+done
+
---
+++ scripts/APN/Traffic.sh
@@ -0,0 +1,52 @@
+use_multi_gpu=0
+if [ $use_multi_gpu -eq 0 ]; then
+    launch_command="python"
+else
+    launch_command="accelerate launch"
+fi
+
+. "$(dirname "$(readlink -f "$0")")/../globals.sh" # Import shared information from scripts/globals.sh
+
+dataset_name=$(basename "$0" .sh) # file name
+dataset_subset_name=""
+dataset_id=$dataset_name
+get_dataset_info "$dataset_name" "$dataset_subset_name" # Get dataset information from scripts/globals.sh
+
+model_name="$(basename "$(dirname "$(readlink -f "$0")")")" # folder name
+model_id=$model_name
+
+seq_len=96
+label_len=48
+for pred_len in 96 192 336 720; do
+    $launch_command main.py \
+    --is_training 1 \
+    --loss "MSE" \
+    --dropout 0.1 \
+    --d_model 24 \
+    --n_layers 2 \
+    --n_heads 8 \
+    --n_patches_list "24" \
+    --tpatchgnn_te_dim 8 \
+    --task_name "long_term_forecast" \
+    --use_multi_gpu $use_multi_gpu \
+    --dataset_root_path $dataset_root_path \
+    --dataset_file_name "traffic.csv" \
+    --model_id $model_id \
+    --model_name $model_name \
+    --dataset_name $dataset_name \
+    --dataset_id $dataset_id \
+    --features M \
+    --seq_len $seq_len \
+    --label_len $label_len \
+    --pred_len $pred_len \
+    --enc_in $n_variables \
+    --dec_in $n_variables \
+    --c_out $n_variables \
+    --train_epochs 300 \
+    --patience 10 \
+    --val_interval 1 \
+    --itr 5 \
+    --batch_size 1 \
+    --learning_rate 1e-2
+done
+

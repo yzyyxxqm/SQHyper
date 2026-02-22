2bed2f9b43b7bdc51d430e45daf8702daa996b52
Ladbaby <1390265970@qq.com>
2026-02-22 11:26:45 +0800

    fix(scripts):Leddam

--- scripts/Leddam/HumanActivity.sh
+++ scripts/Leddam/HumanActivity.sh
@@ -18,25 +18,25 @@ model_id=$model_name
 seq_len=3000
 for pred_len in 300; do
     $launch_command main.py \
-        --is_training 1 \
-        --collate_fn "collate_fn" \
-        --loss "MSE" \
-        --use_multi_gpu $use_multi_gpu \
-        --dataset_root_path $dataset_root_path \
-        --model_id $model_id \
-        --model_name $model_name \
-        --dataset_name $dataset_name \
-        --dataset_id $dataset_id \
-        --features M \
-        --seq_len $seq_len \
-        --pred_len $pred_len \
-        --enc_in $n_variables \
-        --dec_in $n_variables \
-        --c_out $n_variables \
-        --train_epochs 300 \
-        --patience 10 \
-        --val_interval 1 \
-        --itr 5 \
-        --batch_size 32 \
-        --learning_rate 1e-3
+    --is_training 1 \
+    --collate_fn "collate_fn" \
+    --loss "MSE" \
+    --use_multi_gpu $use_multi_gpu \
+    --dataset_root_path $dataset_root_path \
+    --model_id $model_id \
+    --model_name $model_name \
+    --dataset_name $dataset_name \
+    --dataset_id $dataset_id \
+    --features M \
+    --seq_len $seq_len \
+    --pred_len $pred_len \
+    --enc_in $n_variables \
+    --dec_in $n_variables \
+    --c_out $n_variables \
+    --train_epochs 300 \
+    --patience 10 \
+    --val_interval 1 \
+    --itr 5 \
+    --batch_size 32 \
+    --learning_rate 1e-3
 done

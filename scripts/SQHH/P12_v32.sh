ad8c702f7d50fa6b733d3e4ac62b078f9ea10930
yzyyxxqm <gaoxiangwei@my.swjtu.edu.cn>
2026-05-11 06:29:12 +0800

    tune(SQHH/P12_v32): patience=30 + lr=3e-4 for final P12 push

---
+++ scripts/SQHH/P12_v32.sh
@@ -0,0 +1,52 @@
+use_multi_gpu=0
+if [ $use_multi_gpu -eq 0 ]; then
+    launch_command="python"
+else
+    launch_command="accelerate launch"
+fi
+
+. "$(dirname "$(readlink -f "$0")")/../globals.sh"
+
+dataset_name="P12"
+dataset_subset_name=""
+dataset_id=$dataset_name
+get_dataset_info "$dataset_name" "$dataset_subset_name"
+
+model_name="SQHH"
+model_id="SQHH"
+
+# v3.2: longer patience + smaller lr to push test below 0.295
+seq_len=36
+for pred_len in 3; do
+    $launch_command main.py \
+    --is_training 1 \
+    --collate_fn "collate_fn" \
+    --loss "MSE" \
+    --d_model 256 \
+    --n_layers 2 \
+    --n_heads 2 \
+    --use_multi_gpu $use_multi_gpu \
+    --dataset_root_path $dataset_root_path \
+    --model_id $model_id \
+    --model_name $model_name \
+    --dataset_name $dataset_name \
+    --dataset_id $dataset_id \
+    --features M \
+    --seq_len $seq_len \
+    --pred_len $pred_len \
+    --enc_in $n_variables \
+    --dec_in $n_variables \
+    --c_out $n_variables \
+    --train_epochs 300 \
+    --patience 30 \
+    --val_interval 1 \
+    --itr 1 \
+    --batch_size 32 \
+    --learning_rate 3e-4 \
+    --use_amp 1 \
+    --use_compile 0 \
+    --sqhh_k_a 16 \
+    --sqhh_k_e 32 \
+    --sqhh_spike_floor 0.2 \
+    --sqhh_diag_interval 200
+done
